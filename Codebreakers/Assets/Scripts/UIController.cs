2abffdab8181c49269308ca7f2215751e341e43e
Jaime Martínez Villar <jaimemartinezvillar@uoc.edu>
2020-05-09 18:14:39 +0200

    Main Menu UI with controller

--- Codebreakers/Assets/Scripts/UIController.cs
+++ Codebreakers/Assets/Scripts/UIController.cs
@@ -16,6 +16,14 @@ public class UIController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetAxis("Vertical") != 0 && EventSystem.current.currentSelectedGameObject == null)
+        {
+            if (loadMenu.activeSelf)
+                EventSystem.current.SetSelectedGameObject(firstLoadButton);
+            else
+                EventSystem.current.SetSelectedGameObject(firstMenuButton);
+
+        }
 
         CheckMouse();
         
@@ -53,19 +61,37 @@ public class UIController : MonoBehaviour
 
     public void ToLoadMenu()
     {
-        mainMenu.SetActive(false);
-        EventSystem.current.SetSelectedGameObject(null);
-        loadMenu.SetActive(true);
-        EventSystem.current.SetSelectedGameObject(firstLoadButton);
+        StartCoroutine(CloseMainMenu());
     }
 
     public void ToMainMenu()
     {
-        loadMenu.SetActive(false);
-        EventSystem.current.SetSelectedGameObject(null);
-        mainMenu.SetActive(true);
-        EventSystem.current.SetSelectedGameObject(firstMenuButton);
+        StartCoroutine(CloseLoadMenu());
     }
    
+    IEnumerator CloseMainMenu()
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+
+        yield return new WaitForSeconds(0.3f);
+        mainMenu.SetActive(false);
+        
+        loadMenu.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(firstLoadButton);
+       
+    }
+
+    IEnumerator CloseLoadMenu()
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+
+        yield return new WaitForSeconds(0.2f);
+        loadMenu.SetActive(false);
+
+        
+        mainMenu.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(firstMenuButton);
+       
+    }
 
 }
